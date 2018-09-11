67459b09798d4e40ce6dc09c9ac0db1bcc70a49a
aissa <aissa@ARQ>
2018-09-11 21:42:00 +0100

    Add JSTL Support

--- src/main/java/bootwildfly/app/controller/WildFlyController.java
+++ src/main/java/bootwildfly/app/controller/WildFlyController.java
@@ -1,13 +1,19 @@
 package bootwildfly.app.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WildFlyController {
 
+	@Value("${spring.application.name}")
+	private String appName;
+
 	@GetMapping("/")
-	public String sayHello() {
+	public String sayHello(Model m) {
+		m.addAttribute("appName", appName);
 		return "slash";
 	}
 }
--- src/main/webapp/WEB-INF/slash.jsp
+++ src/main/webapp/WEB-INF/slash.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+    
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +11,6 @@
 </head>
 <body>
 <h1>JSP 101</h1>
+<h2>spring.application.name=${appName} </h2>
 </body>
 </html>
