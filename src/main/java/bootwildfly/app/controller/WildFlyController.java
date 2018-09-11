1d93175e3ab18663562820f6350062c34cf7935a
aissa <aissa@ARQ>
2018-09-11 21:21:36 +0100

    Hello World 0.06

--- src/main/java/bootwildfly/app/controller/WildFlyController.java
+++ src/main/java/bootwildfly/app/controller/WildFlyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WildFlyController {
 
-	@GetMapping("/")
+	@GetMapping("/slash")
 	public String sayHello() {
 		return "slash";
 	}
