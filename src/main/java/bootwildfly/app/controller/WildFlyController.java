1a4dc216a81423f8c9cf2b7525f100293c872815
aissa <aissa@ARQ>
2018-09-11 21:19:16 +0100

    Hello World 0.05

---
+++ src/main/java/bootwildfly/app/controller/WildFlyController.java
@@ -0,0 +1,13 @@
+package bootwildfly.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WildFlyController {
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "slash";
+	}
+}
---
+++ src/main/webapp/slash.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>JSP 101</title>
+</head>
+<body>
+<h1>JSP 101</h1>
+</body>
+</html>
