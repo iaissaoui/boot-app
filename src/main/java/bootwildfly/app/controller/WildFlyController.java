b04b3bafb8c118c9fb9848397f94156296f6a132
ia <aissaoui.iqbal@outlook.com>
2018-09-16 01:08:15 +0100

    Postgresql Support

--- src/main/java/bootwildfly/app/controller/WildFlyController.java
+++ src/main/java/bootwildfly/app/controller/WildFlyController.java
@@ -1,34 +1,30 @@
 package bootwildfly.app.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import bootwildfly.app.model.AppUser;
-import bootwildfly.app.repo.AppUserRepo;
 
 @Controller
 public class WildFlyController {
 
 	@Value("${spring.application.name}")
 	private String appName;
-	@Value("IntelliJ")
+	@Value("")
 	private String ide;
 
-	@Autowired
-	AppUserRepo AUR;
+//	@Autowired
+//	AppUserRepo AUR;
 
 	@GetMapping("/")
 	public String sayHello(Model m) {
 
 		m.addAttribute("appName", appName);
 
-		AppUser au = new AppUser();
-		au.setName("ia");
-
-		// AUR.save(au);
+//		AppUser au = new AppUser();
+//		au.setName("ia");
+//
+//		AUR.save(au);
 
 		return "slash";
 	}
