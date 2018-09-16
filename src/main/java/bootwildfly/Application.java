1946b9b357f4da9e5f9b91383122e4c7df6ed406
ia <aissaoui.iqbal@outlook.com>
2018-09-16 02:57:28 +0100

    PostgresSQL Support

--- src/main/java/bootwildfly/Application.java
+++ src/main/java/bootwildfly/Application.java
@@ -1,18 +1,14 @@
 package bootwildfly;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan("bootwildfly.app.model")
 //@EnableJpaRepositories("bootwildfly.app.repo")
-@EnableAutoConfiguration
 @SpringBootApplication
+@ComponentScan(basePackages = { "bootwildfly.app.model" })
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
