f7eede7493cc6e4fa16bd495a50d96b2445499b2
ia <aissaoui.iqbal@outlook.com>
2018-09-16 17:23:10 +0100

    Switch DataSource

--- src/main/java/bootwildfly/app/model/AppUser.java
+++ src/main/java/bootwildfly/app/model/AppUser.java
@@ -1,17 +1,22 @@
 package bootwildfly.app.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Entity
-
+@Entity
 public @Data @AllArgsConstructor @NoArgsConstructor class AppUser {
 
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-//	@Column(nullable = false)
+	@Column(nullable = false)
 	private String name;
 
 }
