83bae620298a49774a0a48d19d143b9598f45fe6
ia <aissaoui.iqbal@outlook.com>
2018-09-18 00:19:12 +0100

    PostgresSQL Support...

--- src/main/java/bootwildfly/app/model/AppUser.java
+++ src/main/java/bootwildfly/app/model/AppUser.java
@@ -1,16 +1,21 @@
 package bootwildfly.app.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Entity
+@Entity
+@Table(name = "appuser")
 public @Data @AllArgsConstructor @NoArgsConstructor class AppUser {
 
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
 	private Long id;
-//	@Column(nullable = false)
+	@Column(nullable = false)
 	private String name;
 
 }
