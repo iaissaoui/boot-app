b1411ab4ae6d89348b97edd4292c476f528682f9
ia <ia@VI>
2018-09-28 15:31:44 +0100

    JPA Test 04

--- src/main/java/bootwildfly/Application.java
+++ src/main/java/bootwildfly/Application.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 //@EnableJpaRepositories("bootwildfly.app.repo")
 //@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@ComponentScan("bootwildfly.app.model")
 @SpringBootApplication
-//@ComponentScan(basePackages = { "bootwildfly.app.model" })
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
